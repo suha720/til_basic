5d5696d2952a864b880a3c0e20f910d72a40ffc6
suha720 <a50914513@gmail.com>
2025-06-05 18:11:29 +0900

    [docs] js 연동 및 swiper 연동 진행중
    2025-06-05

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener(`DOMContentLoaded`, () => {
+  // main
+  const main = document.querySelector(`.main`);
+  // 윈도우 스크롤 체크
+  window.addEventListener(`scroll`, () => {
+    const scY = window.scrollY;
+    if (scY > 0) {
+      main.classList.add(`main_scroll`);
+    } else {
+      main.classList.remove(`main_scroll`);
+    }
+  });
+});
