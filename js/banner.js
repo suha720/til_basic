22f08f893c04be6a384d01b53524ef88049cab18
suha720 <a50914513@gmail.com>
2025-06-10 17:03:39 +0900

    [docs] tour 영역 퍼블리싱 진행 중

--- js/banner.js
+++ js/banner.js
@@ -94,11 +94,27 @@ window.addEventListener("DOMContentLoaded", function () {
       nextEl: ".banner_slide_next",
       prevEl: ".banner_slide_prev",
     },
+    pagination: {
+      el: ".banner_slide_pg",
+      // 페이지네이션 선택해서 이동하기
+      clickable: true,
+    },
     breakpoints: {
       760: {
         slidesPerView: 2,
         spaceBetween: 25,
       },
     },
+  });
+
+  // 아래 코드는 별도로 작성을 한 것입니다. (보관 권장)
+  const banner = document.querySelector(".sw_banner");
+  // 배너 영역에 마우스가 걸친다면
+  banner.addEventListener("mouseenter", () => {
+    swiper.autoplay.stop();
+  });
+  // 배너 영역에 마우스가 빠져나간다면
+  banner.addEventListener("mouseleave", () => {
+    swiper.autoplay.start();
   });
 });
